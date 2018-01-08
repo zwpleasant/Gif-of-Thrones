9379552748f148788969c3da35b2b6e2064a0c49
Pleasant <zpleasant@anl.gov>
2018-01-08 10:37:28 -0600

    added AJAX call and console logged the response of each button click

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -7,9 +7,27 @@ $(document).ready(function() {
   for (var i = 0; i < topics.length; i++) {
     var charButton = $("<button>");
     charButton.addClass("btn-warning");
-    charButton.attr("data-character", topics[i]);
+    charButton.attr("data-name", topics[i]);
     charButton.text(topics[i]);
     $(".buttons").append(charButton);
   }
+  // create on click function to grab 10 non-animated gif images from the GIPHY API and displays them on the page
+  $(".buttons").on("click", function() {
+    // clear the div from previous Gifs
+    $(".gifDisplay").empty();
+
+    // create variable to hold gifphy URL
+    var character = $(this).attr("data-name");
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + character + "&api_key=mAuau4GXKo9SjuYYPFLo7OsO5KfWXHAb";
+
+    // create the AJAX call
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    }).done(function(response) {
+      console.log(response);
+    });
+
+  });
 
 });
