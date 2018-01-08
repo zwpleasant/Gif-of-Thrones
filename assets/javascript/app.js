88dcb4c34d5e4c90773cd4796412f422e4e2d9ae
Pleasant <zpleasant@anl.gov>
2018-01-08 08:57:31 -0600

    added file structure and topics variable in js

---
+++ assets/javascript/app.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+
+  //create an array of strings related to topic
+  var topics = ["Jon Snow", "Daenerys Targaryen", "Cersei Lannister", "Jaime Lannister", "Tyrion Lannister", "Arya Stark", "Sansa Stark"]
+
+  console.log(topics);
+
+});
