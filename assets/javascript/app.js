555e1b5b34aff85ece6388b88e9973d4861f1219
Pleasant <zpleasant@anl.gov>
2018-01-08 12:14:42 -0600

    added functionality of gif generation, but need to refine gif search

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,22 +3,23 @@ $(document).ready(function() {
   //create an array of strings related to topic
   var topics = ["Jon Snow", "Daenerys Targaryen", "Cersei Lannister", "Jaime Lannister", "Tyrion Lannister", "Arya Stark", "Sansa Stark"]
 
-  // make a button for each of the characters within the topic variable
-  for (var i = 0; i < topics.length; i++) {
-    var charButton = $("<button>");
-    charButton.addClass("btn-warning");
-    charButton.attr("data-name", topics[i]);
-    charButton.text(topics[i]);
-    $(".buttons").append(charButton);
-  }
+  // use a function to make a button for each of the characters within the topic variable
+    for (var i = 0; i < topics.length; i++) {
+      var charButton = $("<button>");
+      charButton.addClass("btn-warning");
+      charButton.attr("data-name", topics[i]);
+      charButton.text(topics[i]);
+      $(".buttons").append(charButton);
+    }
+
   // create on click function to grab 10 non-animated gif images from the GIPHY API and displays them on the page
   $(".buttons").on("click", function() {
     // clear the div from previous Gifs
-    $(".gifDisplay").empty();
+    $(".display").empty();
 
     // create variable to hold gifphy URL
-    var character = $(this).attr("data-name");
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + character + "&api_key=mAuau4GXKo9SjuYYPFLo7OsO5KfWXHAb";
+    var character = $(charButton).attr("data-name");
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + character + "&api_key=mAuau4GXKo9SjuYYPFLo7OsO5KfWXHAb&limit=10"; // the addition of &limit=10 limits the serach parameter to 10 gifs
 
     // create the AJAX call
     $.ajax({
@@ -26,6 +27,26 @@ $(document).ready(function() {
       method: "GET"
     }).done(function(response) {
       console.log(response);
+
+      // create variables for needed info
+      var results = response.data;
+
+      // for loop that will go through searchResults and add the gifs to gifDisplay
+      for (var j = 0; j < results.length; j++) {
+        var gifDiv = $("<div class='item'>"); // creates div for each of the ratings and gifs
+        var rating = results[j].rating; // store the rating of the gif
+        var p = $("<p>").text("Rating: " + rating); // creates a paragraph with rating attached
+        var gifImage = $("<img>"); // creates an image tag for the gif result to go into
+        gifImage.attr("src", results[j].images.fixed_height.url);
+
+        // appends paragraph and gif to the gifDiv
+        gifDiv.append(p);
+        gifDiv.append(gifImage);
+
+        // prepends the gifDiv to the original div from the html
+        $(".display").prepend(gifDiv);
+      };
+
     });
 
   });
